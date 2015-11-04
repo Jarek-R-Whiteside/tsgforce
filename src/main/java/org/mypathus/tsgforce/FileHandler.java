088014fc37995db75c4b14027350eba8829ad956
Jarek-R-Whiteside <Jarek.R.Whiteside@tsgforce.com>
2015-11-04 15:14:21 -0500

    Added the work we did in the Mob Programming, building upon Guillermo's effort  at file reading.  This commit has updates to how Excel headers are handled in xls and xlsx.

--- src/main/java/org/mypathus/tsgforce/FileHandler.java
+++ src/main/java/org/mypathus/tsgforce/FileHandler.java
@@ -1,6 +1,13 @@
 package org.mypathus.tsgforce;
 
 import java.io.BufferedReader;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -10,7 +17,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Iterator;
 import org.apache.tika.Tika;
 
 public class FileHandler {
@@ -19,7 +26,26 @@ public class FileHandler {
 	
 	public static void main(String[] args) {
 		FileHandler fh = new FileHandler();
-		fh.getFileHeaders("SampleFileText1.txt");
+		
+//		String realFileName = fh.getFileType("SampleFileExcel.xls");
+//		System.out.println(realFileName);
+//		
+//		String realFileName2 = fh.getFileType("SampleFileExcel.xlsx");
+//		System.out.println(realFileName2);
+//		
+//		String realFileName3 = fh.getFileType("SampleFileTiff.tiff");
+//		System.out.println(realFileName3);
+//		
+//		String realFileName4 = fh.getFileType("SampleFileText1.txt");
+//		System.out.println(realFileName4);
+//		
+		try {
+			fh.getExcelHeaders2007("SampleFileExcel.xlsx");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public String getFileType(String fileName) {
@@ -76,9 +102,78 @@ public class FileHandler {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		System.out.println(headersLine);
+		
 		return headersLine;
 	}
 	
-
+	public void getExcelHeaders2003(String fileName) throws IOException{
+		Path path = Paths.get(fileDirectory + fileName);
+		  FileInputStream inputStream = new FileInputStream(new File (path.toString()));
+	         
+	        Workbook workbook = new HSSFWorkbook(inputStream);
+	        Sheet firstSheet = workbook.getSheetAt(0);
+	        Iterator<Row> iterator = firstSheet.iterator();
+	         
+	            Row nextRow = iterator.next();
+	            Iterator<Cell> cellIterator = nextRow.cellIterator();
+	             
+	            while (cellIterator.hasNext()) {
+	                Cell cell = cellIterator.next();
+	                 
+	                switch (cell.getCellType()) {
+	                    case Cell.CELL_TYPE_STRING:
+	                        System.out.print(cell.getStringCellValue());
+	                        break;
+	                    case Cell.CELL_TYPE_BOOLEAN:
+	                        System.out.print(cell.getBooleanCellValue());
+	                        break;
+	                    case Cell.CELL_TYPE_NUMERIC:
+	                        System.out.print(cell.getNumericCellValue());
+	                        break;
+	                }
+	                System.out.print(" - ");
+	            }
+	            System.out.println();
+	        
+	         
+	        workbook.close();
+	        inputStream.close();
+	    }
 	
+	public void getExcelHeaders2007(String fileName) throws IOException{
+		Path path = Paths.get(fileDirectory + fileName);
+		  FileInputStream inputStream = new FileInputStream(new File (path.toString()));
+	         
+	        Workbook workbook = new XSSFWorkbook(inputStream);
+	        Sheet firstSheet = workbook.getSheetAt(0);
+	        Iterator<Row> iterator = firstSheet.iterator();
+	         
+	            Row nextRow = iterator.next();
+	             nextRow = iterator.next();
+	             nextRow = iterator.next();
+	            Iterator<Cell> cellIterator = nextRow.cellIterator();
+	             
+	            while (cellIterator.hasNext()) {
+	                Cell cell = cellIterator.next();
+	                 
+	                switch (cell.getCellType()) {
+	                    case Cell.CELL_TYPE_STRING:
+	                        System.out.print(cell.getStringCellValue());
+	                        break;
+	                    case Cell.CELL_TYPE_BOOLEAN:
+	                        System.out.print(cell.getBooleanCellValue());
+	                        break;
+	                    case Cell.CELL_TYPE_NUMERIC:
+	                        System.out.print(cell.getNumericCellValue());
+	                        break;
+	                }
+	                System.out.print(" - ");
+	            }
+	            System.out.println();
+	        
+	         
+	        workbook.close();
+	        inputStream.close();
+	    }
 }
