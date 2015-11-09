364ef19440ad71c5f6071717f60711afc4dd0976
RobCFFG <robert.carter@tsgforce.com>
2015-11-09 15:18:00 -0500

    Example of a report data class

---
+++ src/main/java/org/mypathus/tsgforce/Report01.java
@@ -0,0 +1,23 @@
+package org.mypathus.tsgforce;
+
+public class Report01 {
+	
+	String employer;
+	
+	
+	
+	public String getEmployer() {
+		return employer;
+	}
+	public void setEmployer(String employer) {
+		this.employer = employer;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	double balance;
+
+}
