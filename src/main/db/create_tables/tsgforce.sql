fedb4a786d2d597d681bcabe2a836921bde9f597
trevor4 <trevor.b.smith@tsgforce.com>
2015-12-01 17:05:18 -0500

    added script for report and mapping tables

---
+++ src/main/db/create_tables/tsgforce.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 01, 2015 at 04:58 PM
+-- Server version: 5.5.46-0ubuntu0.14.04.2
+-- PHP Version: 5.5.9-1ubuntu4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `tsgforce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mapping`
+--
+
+CREATE TABLE IF NOT EXISTS `mapping` (
+  `report_id` varchar(3) NOT NULL,
+  `field_id` varchar(35) NOT NULL,
+  `start_pos` int(3) NOT NULL,
+  `end_pos` int(3) NOT NULL,
+  `destination` varchar(45) NOT NULL,
+  PRIMARY KEY (`report_id`,`field_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE IF NOT EXISTS `report` (
+  `report_id` varchar(3) NOT NULL,
+  `file_type` varchar(4) NOT NULL,
+  `header` varchar(4000) NOT NULL,
+  `header_row` int(6) NOT NULL,
+  PRIMARY KEY (`report_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `mapping`
+--
+ALTER TABLE `mapping`
+  ADD CONSTRAINT `mapping_fk1` FOREIGN KEY (`report_id`) REFERENCES `report` (`report_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
