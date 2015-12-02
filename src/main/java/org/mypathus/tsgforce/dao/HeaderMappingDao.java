c307cb535e155a9d1cee489d438c100f67763736
RobCFFG <robert.carter@tsgforce.com>
2015-12-02 14:21:47 -0500

    log4j example added to HeaderMappingDao.java

--- src/main/java/org/mypathus/tsgforce/dao/HeaderMappingDao.java
+++ src/main/java/org/mypathus/tsgforce/dao/HeaderMappingDao.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.mypathus.tsgforce.model.ReportIdentificationHelper;
 import org.mypathus.tsgforce.processing.Excel2003File;
 import org.mypathus.tsgforce.processing.Excel2007File;
@@ -16,7 +17,8 @@ public class HeaderMappingDao {
 	static ReportIdentificationHelper helperXls1 = new ReportIdentificationHelper("application/vnd.ms-excel", "Uniqe IDUsernameFirst NameLast NameProgram SiteTotal no. of loginsLength of loginsIs Expense Tracker completedIs My Budget completedIs Saving Generator completed", "myPathOnlineSample", 1);
 	static ReportIdentificationHelper helperXls2 = new ReportIdentificationHelper("application/vnd.ms-excel", "UsernameFirst NameLast NameProgram SiteTotal no. of loginsLength of loginsIs Expense Tracker completedIs My Budget completedIs Saving Generator completed", "consolidatedUsersInformation", 2);
 	static ReportIdentificationHelper helperXlsx1 = new ReportIdentificationHelper("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet/vnd.ms-excel", "Q1Q2Q3Q4Q5Q6Q7Q8Q9Q10Q11Q12Q13Q14Q15Q16Q17Q18Q19.Q20.Q21.Q22.Q23.ab.c.d.e.Q24.ab.c.d.e.f.g.Q25.ab.c.d.e.f.g.Q26.ab.c.d.e.f.Q27.abcdefghijQ28.abcQ29.abcdefghQ30Q31Q32Q33a.bcdefghijklmnopqrst", "survey", 2);
-
+	final static Logger logger = Logger.getLogger(Class.class);
+	
 	private static List<ReportIdentificationHelper> identificationHelpers = Arrays.asList(helperTxt1, helperTxt2, helperXls1, helperXls2, helperXlsx1);
 	
 	public static String getTextReportLayout(String fileName) {
@@ -48,7 +50,8 @@ public class HeaderMappingDao {
 						return helper.getName();
 					} 
 				} catch (IOException e) {
-				e.printStackTrace();
+				//e.printStackTrace();
+					logger.error("Unable to read excel 2003 file: " + fileName);
 				}
 			}
 		}
@@ -68,7 +71,8 @@ public class HeaderMappingDao {
 						return helper.getName();
 					} 
 				} catch (IOException e) {
-				e.printStackTrace();
+				//e.printStackTrace();
+					logger.error("Unable to read xslx excel file: " + fileName);
 				}
 			}
 		}
